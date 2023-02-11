4bea456900bcd99507315cdaa7eeffee75e7d70f
Artem Lukashov <artem.lukashov1@gmail.com>
2023-02-11 14:51:08 +0100

    Решение 34 задачи

---
+++ my34/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 34: Задайте массив заполненный
+//случайными положительными трёхзначными числами.
+//Напишите программу, которая покажет количество чётных чисел в массиве.
+
+//[345, 897, 568, 234] -> 2
+
+Console.Clear();
+
+int[] Create(int N, int min, int max) {  
+      int[] A = new int[N];
+    for (int i = 0; i < N; i++)
+        A[i] = new Random().Next(min,max);
+    return A;
+    }
+
+int EvenNum(int[] A){
+    int count=0;
+    for (int i=0; i<(A.Length-1); i++) {
+        if (A[i]%2==0) 
+            count+=1;
+        }
+    return count;
+    }
+Console.WriteLine("Please enter the quantity: "); 
+int N= int.Parse(Console.ReadLine());
+Console.WriteLine("Created array: ");
+
+int[] Arr = Create(N,100,1000);
+Console.WriteLine($"[{String.Join(", ", Arr)}]");
+
+Console.WriteLine($"The quantity of Even numbers is: {EvenNum(Arr)}");
